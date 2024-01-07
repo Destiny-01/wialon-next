fbfaa23ec893216b9aa16019db6bef6e17945eed
Destiny Aigbe <aigbedestinyic@gmail.com>
2024-01-07 22:19:57 +0100

    fixed some reports not coming up

--- src/components/table.js
+++ src/components/table.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function timeToSeconds(time) {
-  const [hours, minutes, seconds] = time.split(':').map(Number);
+  if (!time) {
+    return 0;
+  }
+  const [hours, minutes, seconds] = time.split(":").map(Number);
 
   const totalSeconds = hours * 3600 + minutes * 60 + seconds;
 
@@ -25,52 +28,53 @@ const DriverTable = ({ tableData }) => {
       <div>
         <h1>Driver Performance Table</h1>
 
-        <div className='table-responsive scrollable-table'>
-          <table className='table table-bordered table-dark'>
+        <div className="table-responsive scrollable-table">
+          <table className="table table-bordered table-dark">
             <thead>
               <tr>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Driver Name
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Driver Score
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Distance
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Max Speed
                 </th>
-                <th scope='col' colSpan='4'>
+                <th scope="col" colSpan="4">
                   Violation count
                 </th>
-                <th scope='col' colSpan='4'>
+                <th scope="col" colSpan="4">
                   Driver Score Component
                 </th>
               </tr>
               <tr>
-                <th scope='col'>Accelaration</th>
-                <th scope='col'>Brake</th>
-                <th scope='col'>Driving Hours</th>
-                <th scope='col'>Speed</th>
-                <th scope='col'>Accelaration</th>
-                <th scope='col'>Brake</th>
-                <th scope='col'>Driving Hours</th>
-                <th scope='col'>Speed</th>
+                <th scope="col">Accelaration</th>
+                <th scope="col">Brake</th>
+                <th scope="col">Driving Hours</th>
+                <th scope="col">Speed</th>
+                <th scope="col">Accelaration</th>
+                <th scope="col">Brake</th>
+                <th scope="col">Driving Hours</th>
+                <th scope="col">Speed</th>
               </tr>
             </thead>
             <tbody>
               {tableData.map((table, i) => (
                 <tr key={i}>
-                  <th scope='row'>{table.c[18] || '----'}</th>
-                  <td>{table.c[9] || '----'}</td>
+                  <th scope="row">{table.c[18] || "----"}</th>
+                  <td>{table.c[9] || "----"}</td>
                   <td>
                     {(
-                      parseFloat(table.c[16]) - parseFloat(table.c[15])
-                    ).toFixed(2)}{' '}
+                      (parseFloat(table.c[16]) || 0) -
+                      (parseFloat(table.c[15]) || 0)
+                    ).toFixed(2) || "----"}{" "}
                     km
                   </td>
-                  <td>{table.c[7] || '----'}</td>
+                  <td>{table.c[7] || "----"}</td>
                   <td>---</td>
                   <td>---</td>
                   <td>{timeToSeconds(table.c[13]) || 0}</td>
@@ -78,7 +82,7 @@ const DriverTable = ({ tableData }) => {
                     {calculateSpeed(
                       parseFloat(table.c[14]),
                       timeToSeconds(table.c[13])
-                    ) || 0}{' '}
+                    ) || 0}{" "}
                     km/h
                   </td>
                   <td>---</td>
@@ -91,7 +95,7 @@ const DriverTable = ({ tableData }) => {
                       ).toFixed(2) * 3600
                     )}
                   </td>
-                  <td>{table.c[6]}</td>
+                  <td>{table.c[6] || "0 km/h"}</td>
                 </tr>
               ))}
             </tbody>
