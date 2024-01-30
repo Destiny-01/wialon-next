878cba56e64a38abd5b392ff7a8eeceb5715531c
Destiny Aigbe <aigbedestinyic@gmail.com>
2024-01-30 22:51:53 +0100

    added violations

--- src/components/table.js
+++ src/components/table.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function timeToSeconds(time) {
   if (!time) {
     return 0;
   }
-  const [hours, minutes, seconds] = time.split(':').map(Number);
+  const [hours, minutes, seconds] = time.split(":").map(Number);
 
   const totalSeconds = hours * 3600 + minutes * 60 + seconds;
 
@@ -22,62 +22,120 @@ function calculateSpeed(distance, timeInSeconds) {
   return speed.toFixed(2);
 }
 
+function timeStringToSeconds(timeString) {
+  const [hours, minutes, seconds] = timeString.split(":").map(Number);
+  return hours * 3600 + minutes * 60 + seconds;
+}
+
+// Function to convert seconds to time string
+function secondsToTimeString(seconds) {
+  if (!seconds) {
+    return null;
+  }
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+    2,
+    "0"
+  )}:${String(remainingSeconds).padStart(2, "0")}`;
+}
+
+// Calculate average time
+function calculateAverageTime(timeArray) {
+  const totalSeconds = timeArray.reduce(
+    (acc, timeString) => acc + timeStringToSeconds(timeString[4]),
+    0
+  );
+  const averageSeconds = totalSeconds / timeArray.length;
+
+  return secondsToTimeString(averageSeconds);
+}
+
+function calculateAverageSpeed(speedArray) {
+  const totalSeconds = speedArray.reduce(
+    (acc, speedString) => acc + parseFloat(speedString[5]),
+    0
+  );
+  const averageSeconds = totalSeconds / speedArray.length;
+
+  return averageSeconds ? averageSeconds.toFixed(2) + " km" : null;
+}
+
+const computeViolations = (violations) => {
+  if (!violations) return null;
+  let speeding = violations.filter((viol) =>
+    viol[0]?.toLowerCase().startsWith("speed")
+  );
+  let braking = violations.filter((viol) =>
+    viol[0]?.toLowerCase().startsWith("bra")
+  );
+
+  return (
+    <>
+      <td>{speeding?.length || "----"}</td>
+      <td>{braking?.length || "----"}</td>
+      <td>{calculateAverageTime(violations) || "----"}</td>
+      <td>{calculateAverageSpeed(violations) || "----"}</td>
+    </>
+  );
+};
+
 const DriverTable = ({ tableData }) => {
   console.log(tableData);
+
   return (
     <div>
       <div>
         <h1>Driver Performance Table</h1>
 
-        <div className='table-responsive scrollable-table'>
-          <table className='table table-bordered table-dark'>
+        <div className="table-responsive scrollable-table">
+          <table className="table table-bordered table-dark">
             <thead>
               <tr>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Driver Name
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Driver Score
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Distance
                 </th>
-                <th scope='col' rowSpan='2'>
+                <th scope="col" rowSpan="2">
                   Max Speed
                 </th>
-                <th scope='col' colSpan='4'>
+                <th scope="col" colSpan="4">
                   Violation count
                 </th>
-                <th scope='col' colSpan='4'>
+                <th scope="col" colSpan="4">
                   Driver Score Component
                 </th>
               </tr>
               <tr>
-                <th scope='col'>Accelaration</th>
-                <th scope='col'>Brake</th>
-                <th scope='col'>Driving Hours</th>
-                <th scope='col'>Speed</th>
-                <th scope='col'>Accelaration</th>
-                <th scope='col'>Brake</th>
-                <th scope='col'>Driving Hours</th>
-                <th scope='col'>Speed</th>
+                <th scope="col">Accelaration</th>
+                <th scope="col">Brake</th>
+                <th scope="col">Driving Hours</th>
+                <th scope="col">Speed</th>
+                <th scope="col">Accelaration</th>
+                <th scope="col">Brake</th>
+                <th scope="col">Driving Hours</th>
+                <th scope="col">Speed</th>
               </tr>
             </thead>
             <tbody>
               {tableData[1]?.map((table, i) => (
                 <tr key={i}>
-                  <th scope='row'>{table.c[0] || '----'}</th>
-                  <td>{table.c[5] || '----'}</td>
-                  <td>{table.c[2] || '----'}</td>
-                  <td>{table.c[6]?.t || '0 km/h'}</td>
-                  <td>{table.c[8] || '----'}</td>
-                  <td>{table.c[10] || '----'}</td>
-                  <td>{table.c[8] || '----'}</td>
-                  <td>{table.c[10] || '----'}</td>
-                  <td>{'----'}</td>
-                  <td>{'----'}</td>
-                  <td>{'----'}</td>
-                  <td>{'----'}</td>
+                  <th scope="row">{table.c[0] || "----"}</th>
+                  <td>{table.c[5] || "----"}</td>
+                  <td>{table.c[2] || "----"}</td>
+                  <td>{table.c[6]?.t || "0 km/h"}</td>
+                  {computeViolations(table.c[table.c.length - 1]?.b)}
+                  <td>{"----"}</td>
+                  <td>{"----"}</td>
+                  <td>{"----"}</td>
+                  <td>{"----"}</td>
                 </tr>
               ))}
             </tbody>
--- src/pages/api/wialon.js
+++ src/pages/api/wialon.js
@@ -1,179 +1,228 @@
-const wialon = require('wialon');
+const wialon = require("wialon");
 
 export default async function handler(req, res) {
-  const opts = {
-    authz: {
-      token:
-        'cff41ecd2f9615c24a95c8e9d906cde9DFC283DDD9407133F3B10D5E589A8419681732CF',
-    },
-  };
-
-  const session = await wialon(opts).session;
-
-  if (req.method === 'POST' && req.body.first === 'wialon_first') {
-    let response;
-    const params = {
-      params: [
-        {
-          svc: 'core/search_items',
-          params: {
-            spec: {
-              itemsType: 'avl_resource',
-              propName: '*',
-              propValueMask: '*',
-              sortType: '',
-            },
-            force: 1,
-            flags: 1,
-            from: 0,
-            to: 4294967295,
-          },
-        },
-        {
-          svc: 'core/update_data_flags',
-          params: {
-            spec: [
-              { type: 'type', data: 'avl_resource', flags: 33281, mode: 1 },
-            ],
-          },
-        },
-        {
-          svc: 'core/update_data_flags',
-          params: {
-            spec: [
-              { type: 'type', data: 'avl_resource', flags: 8197, mode: 1 },
-            ],
-          },
-        },
-      ],
-      flags: 0,
+  try {
+    const opts = {
+      authz: {
+        token:
+          "cff41ecd2f9615c24a95c8e9d906cde9DFC283DDD9407133F3B10D5E589A8419681732CF",
+      },
     };
 
-    await session
-      .request('core/batch', params)
-      .then(async function (data) {
-        response = data;
-        ///console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
+    const session = await wialon(opts).session;
+
+    if (req.method === "POST" && req.body.first === "wialon_first") {
+      let response;
+
+      const params = {
+        params: [
+          {
+            svc: "core/search_items",
+            params: {
+              spec: {
+                itemsType: "avl_resource",
+                propName: "*",
+                propValueMask: "*",
+                sortType: "",
+              },
+              force: 1,
+              flags: 1,
+              from: 0,
+              to: 4294967295,
+            },
+          },
+          {
+            svc: "core/update_data_flags",
+            params: {
+              spec: [
+                { type: "type", data: "avl_resource", flags: 33281, mode: 1 },
+              ],
+            },
+          },
+          {
+            svc: "core/update_data_flags",
+            params: {
+              spec: [
+                { type: "type", data: "avl_resource", flags: 8197, mode: 1 },
+              ],
+            },
+          },
+        ],
+        flags: 0,
+      };
+
+      await session
+        .request("core/batch", params)
+        .then(async function (data) {
+          response = data;
+          ///console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+        });
+
+      return res.status(200).json({
+        response,
+      });
+    } else if (req.method === "POST" && req.body.first === "wialon_second") {
+      let response;
+      console.log("starting the request", req.body.params);
+
+      await session
+        .request("report/exec_report", req.body.params)
+        .then(function (data) {
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+          // log.innerHTML = JSON.stringify(err);
+        });
+
+      await session
+        .request("report/get_report_status", {})
+
+        .then(async function (data) {
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+        });
+
+      await session
+        .request("events/check_updates", { detalization: 3 })
+        .then(async function (data) {
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+        });
+
+      await session
+        .request("report/get_report_status", {})
+        .then(async function (data) {
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+        });
+
+      await session
+        .request("report/apply_report_result", {})
+        .then(async function (data) {
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+          return;
+        });
+
+      const table = (id) => ({
+        tableIndex: id,
+        config: {
+          type: "range",
+          data: { from: 0, to: 30, level: 0, unitInfo: 1 },
+        },
       });
 
-    return res.status(200).json({
-      response,
-    });
-  } else if (req.method === 'POST' && req.body.first === 'wialon_second') {
-    let response;
-    console.log('starting the request', req.body.params);
+      const tables = await Promise.allSettled([
+        makeRequest(session, table(-1)),
+        makeRequest(session, table(0)),
+        makeRequest(session, table(1)),
+        makeRequest(session, table(2)),
+        makeRequest(session, table(3)),
+        makeRequest(session, table(4)),
+        makeRequest(session, table(5)),
+        makeRequest(session, table(6)),
+      ]);
 
-    await session
-      .request('report/exec_report', req.body.params)
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
-        // log.innerHTML = JSON.stringify(err);
-      });
+      function makeRequest(session, table) {
+        return new Promise(async (resolve) => {
+          try {
+            const result = await session.request(
+              "report/select_result_rows",
+              table
+            );
+            resolve({ status: "fulfilled", value: result });
+          } catch (error) {
+            resolve({ status: "rejected", reason: error.message });
+          }
+        });
+      }
 
-    await session
-      .request('report/get_report_status', {})
+      const output = tables.map(
+        (table) => table.status === "fulfilled" && table.value.value
+      );
 
-      .then(async function (data) {
-        console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
-      });
+      console.log(output);
+      // let output1 = {};
 
-    await session
-      .request('events/check_updates', { detalization: 3 })
-      .then(async function (data) {
-        console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
-      });
+      async function processTable(table, i) {
+        if (table.c[6] < 10) {
+          const requestTable = {
+            tableIndex: 5,
+            config: {
+              type: "row",
+              data: { rows: [i], level: 0, unitInfo: 1 },
+            },
+          };
 
-    await session
-      .request('report/get_report_status', {})
-      .then(async function (data) {
-        console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
-      });
-
-    await session
-      .request('report/apply_report_result', {})
-      .then(async function (data) {
-        console.log(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-        return;
-      });
-
-    const table = (id) => ({
-      tableIndex: id,
-      config: {
-        type: 'range',
-        data: { from: 0, to: 30, level: 0, unitInfo: 1 },
-      },
-    });
-
-    const tables = await Promise.allSettled([
-      makeRequest(session, table(-1)),
-      makeRequest(session, table(0)),
-      makeRequest(session, table(1)),
-      makeRequest(session, table(2)),
-      makeRequest(session, table(3)),
-      makeRequest(session, table(4)),
-      makeRequest(session, table(5)),
-      makeRequest(session, table(6)),
-    ]);
-
-    function makeRequest(session, table) {
-      return new Promise(async (resolve) => {
-        try {
           const result = await session.request(
-            'report/select_result_rows',
-            table
+            "report/select_result_rows",
+            requestTable
           );
-          resolve({ status: 'fulfilled', value: result });
-        } catch (error) {
-          resolve({ status: 'rejected', reason: error.message });
+
+          result.forEach((r) => {
+            output[1] = output[1].map((o) => {
+              o.c.push({ b: [] });
+              console.log(o.c[5] < 10, o.c[0], r.c[3]);
+              parseFloat(o.c[5]) < 10 &&
+                o.c[0].trim() === r.c[3].trim() &&
+                o.c[o.c.length - 1].b.push(r.c);
+              return o;
+            });
+          });
         }
+      }
+
+      // Use Promise.all to await all asynchronous operations
+      await Promise.all(output[6]?.map(processTable));
+
+      console.log(output[1][8]?.c, "nnn");
+
+      // await session
+      //   .request('report/select_result_rows', req.body.table)
+      //   .then(async function (data) {
+      //     console.log(data);
+      //     response = data;
+      //   })
+      //   .catch(function (err) {
+      //     console.log(err);
+      //     return;
+      //   });
+
+      return res.status(200).json({
+        // response,
+        response: output,
       });
+      // } else if (req.method === "PUT") {
+      //   const { tableIndex, row } = req.body;
+      //   const table = {
+      //     tableIndex,
+      //     config: { type: "row", data: { rows: [row], level: 0, unitInfo: 1 } },
+      //   };
+
+      //   const result = await session.request("report/select_result_rows", table);
+
+      //   return res.status(200).json({ result });
+    } else {
+      return res.status(400).json({ error: "Invalid route" });
     }
-
-    const output = tables.map(
-      (table) => table.status === 'fulfilled' && table.value.value
-    );
-
-    console.log(output);
-
-    // await session
-    //   .request('report/select_result_rows', req.body.table)
-    //   .then(async function (data) {
-    //     console.log(data);
-    //     response = data;
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //     return;
-    //   });
-
-    return res.status(200).json({
-      // response,
-      response: output,
-    });
-  } else {
-    return res.status(400).json({ error: 'Invalid route' });
+  } catch (err) {
+    console.log("Error", err);
   }
 }
